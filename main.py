b014dd4ee57a7a9c010b1d2318ed33e038ff63ea
yossefaz <yossefaz@protonmail.com>
2020-11-27 07:59:12 +0200

    refactor elements unpacking

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ RESOURCE_DIR = "resources"
 OUTPUT_DIR = "output"
 OUTPUT_CSV_NAME = "global.csv"
 
+def generate_name(name_string:str)-> list:
+    name = name_string.split(" ")
+    if len(name) == 1:
+        name.append('')
+    return name
+
+def generate_town(town_string:str)->list:
+    town = town_string.split(" ")
+    if len(town) == 1:
+        town.append('')
+    return town
 
 def generate_adress_list() -> list:
     address_global = []
@@ -15,14 +26,16 @@ def generate_adress_list() -> list:
             fichier = os.path.join(RESOURCE_DIR, filename)
             with open(fichier, 'r', encoding='utf-8') as address_txt_file:
                 address_list_local = []
+                phones = [None,None]
                 for line in address_txt_file:
                     if line == "\n":
                         try:
-                            name = ' '.join(address_list_local[1:-2])
+                            nom, prenom = generate_name(address_list_local[0])
+                            clinique = ' '.join(address_list_local[1:-2])
                             telephone = address_list_local[0]
                             address = address_list_local[-2]
-                            town = address_list_local[-1]
-                            address_list_local = [name, telephone, address, town]
+                            zipcode, town_name = generate_town(address_list_local[-1])
+                            address_list_local = [nom,prenom,clinique, *phones, address, town_name, zipcode]
                             address_global.append(address_list_local)
                             address_list_local = []
                         except:
@@ -32,7 +45,11 @@ def generate_adress_list() -> list:
                         address_list_local.append(line)
                     elif is_tel(line):
                         line = remove_non_digit(line)
-                        address_list_local.insert(0, line)
+                        if(phones[0] is None):
+                            phones[0] = line
+                        elif(phones[1] is None) :
+                            phones[1] = line
+
     return address_global
 
 
@@ -40,7 +57,7 @@ def generate_csv(address_list: list):
     out_csv_path = os.path.join(OUTPUT_DIR, OUTPUT_CSV_NAME)
     with open(out_csv_path, 'w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(["Nom", "Telephone", "Adresse", "ville", "lat", "lon"])
+        writer.writerow(["Nom", "Prenom", "nom_clinique", "Telephone_1", "Telephone_1", "Adresse", "ville", "code_postal","lat", "lon"])
         count = 0
         total = len(address_list)
         for line in address_list:
